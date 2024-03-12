c811d085dd83b4ff715197d8e9bb9e7a33cf54db
Nemanja <nemanja.vasic@productdock.com>
2024-03-12 22:59:54 +0100

    Add prop types

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import {useStore} from "jotai";
 import {currentPageAtom} from "../atoms.js";
 
@@ -9,6 +10,10 @@ const Button = ({page}) => {
 		className="pointer-events-auto py-4 px-8 bg-white-400 text-white font-semibold border border-gray-400 rounded shadow cursor-pointer transition-colors duration-500">
 		{page.toUpperCase()}
 	</button>;
+}
+
+Button.propTypes = {
+	page: PropTypes.string
 }
 
 export default Button;
