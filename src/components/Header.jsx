d7c762314ed22dc9fab999dbd04d783355bb91bd
Nemanja <nemanja.vasic@productdock.com>
2024-04-03 22:57:51 +0200

    Add header links

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+const Header = () => (
+  <div>
+    <a href="/">
+      <img src="/initlogo.png" alt="logo" className="w-32 left-10 top-10 absolute z-50" />
+    </a>
+    <a
+      href="https://www.dzobs.com/"
+      rel="noreferrer"
+      target="_blank"
+      className="w-13 right-40 top-10 absolute z-50 text-white font-semibold"
+    >
+      <p>DZOBS</p>
+    </a>
+    <a
+      href="https://www.flickr.com/photos/195738908@N05/albums/72177720299447854/"
+      rel="noreferrer"
+      target="_blank"
+      className="w-12 right-24 top-10 absolute z-50 text-white font-semibold"
+    >
+      <p>INIT 1</p>
+    </a>
+    <a
+      href="https://www.flickr.com/photos/195738908@N05/albums/72177720302433321/"
+      rel="noreferrer"
+      target="_blank"
+      className="w-12 right-10 top-10 absolute z-50 text-white font-semibold"
+    >
+      <p>INIT 2</p>
+    </a>
+  </div>
+);
+
+export default Header;
--- src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-const Logo = () => (
-  <div>
-    <a href="/">
-      <img src="/initlogo.png" alt="logo" className="w-32 left-10 top-10 fixed z-50" />
-    </a>
-  </div>
-);
-
-export default Logo;
