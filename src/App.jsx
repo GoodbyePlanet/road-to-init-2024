06b40826492ea263f3987a215d1633a039f012ef
Nemanja <nemanja.vasic@productdock.com>
2024-08-05 09:23:20 +0200

    Part one

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-import { Suspense } from 'react';
-import { Provider } from 'jotai';
 import { Canvas } from '@react-three/fiber';
-import Scene from './components/Scene.jsx';
-import { Bloom, EffectComposer } from '@react-three/postprocessing';
-import { UI } from './components/UI.jsx';
 import { BACKGROUND_COLOR } from './colors.js';
-import ProgressBar from './components/ProgressBar.jsx';
 
 const App = () => {
   return (
-    <Provider>
-      <Canvas shadows camera={{ position: [0, 0, 8], fov: 45 }}>
-        <color attach="background" args={[BACKGROUND_COLOR]} />
-        <fog attach="fog" args={[BACKGROUND_COLOR, 8, 20]} />
-        <Suspense fallback={<ProgressBar />}>
-          <Scene />
-        </Suspense>
-        <EffectComposer>
-          <Bloom mipmapBlur intensity={1.2} />
-        </EffectComposer>
-      </Canvas>
-      <UI />
-    </Provider>
+    <Canvas shadows camera={{ position: [0, 0, 8], fov: 45 }}>
+      <color attach="background" args={[BACKGROUND_COLOR]} />
+      <fog attach="fog" args={[BACKGROUND_COLOR, 8, 20]} />
+    </Canvas>
   );
 };
 
