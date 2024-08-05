26e0555280e7356ed8d9b79414e9e9e484c7cdf1
Nemanja <nemanja.vasic@productdock.com>
2024-08-05 09:28:59 +0200

    Part1: update scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Canvas } from '@react-three/fiber';
-import { BACKGROUND_COLOR } from './colors.js';
+import { Scene } from './components/Scene.jsx';
 
 const App = () => {
   return (
     <Canvas shadows camera={{ position: [0, 0, 8], fov: 45 }}>
-      <color attach="background" args={[BACKGROUND_COLOR]} />
-      <fog attach="fog" args={[BACKGROUND_COLOR, 8, 20]} />
+      <color attach="background" args={["#ececec"]} />
+      <Scene />
     </Canvas>
   );
 };
